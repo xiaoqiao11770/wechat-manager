8e25e349f523c71ae6c3b8ef583786f30d13c0b8
Jon <xiaoqiao11770@gmail.com>
2017-02-02 23:38:53 +0800

    test_02

--- config.py
+++ config.py
@@ -15,10 +15,10 @@ def default_config(app):
 
 def wechat_config():
     weconf = dict(
-    token='your_token',
-    appid='your_appid',
-    appsecret='your_appsecret',
+    token='test_houdini',
+    appid='wx332e0321845c6e3c',
+    appsecret='e2bb9f5d4af14943d8c0121d61cf50ed',
     encrypt_mode='safe',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
-    encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
+    encoding_aes_key='XUi5VjeFWb7v5LFVIFOwL0iT0fQLTQv3TkLrZfigu6h'  # 如果传入此值则必须保证同时传入 token, appid
     )
     return weconf
