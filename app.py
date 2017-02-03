6700eed76f2ffe2cb503840b98a2bc32f8dfb978
Jon <xiaoqiao11770@gmail.com>
2017-02-03 14:20:49 +0800

    test_05

--- app.py
+++ app.py
@@ -17,9 +17,41 @@ def index():
 def weixin():
     try:
         if request.method == 'GET':
-            return GET()
+            data = request.args
+            print '=' * 20, 'GET', '=' * 20
+            print 'DATA===>>', data
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.get('signature', '')
+            timestamp = data.get('timestamp', '')
+            nonce = data.get('nonce', '')
+            echostr = data.get('echostr', '')
+            token = "test_houdini"
+
+            list = [token, timestamp, nonce]
+            list.sort()
+            sha1 = hashlib.sha1()
+            map(sha1.update, list)
+            hashcode = sha1.hexdigest()
+            print "handle/GET func: hashcode, signature: ", hashcode, signature
+            if hashcode == signature:
+                return echostr
+            else:
+                return ""
         else:
-            return POST()
+            print '=' * 20, 'POSE', '=' * 20
+            webData = request.stream.read()
+            print "Handle Post webdata is ", webData   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
+            else:
+                print "暂且不处理"
+                return "success"
     except Exception, Argument:
         return Argument
 
