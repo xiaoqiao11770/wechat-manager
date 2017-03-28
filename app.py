878b1096547ebda782efb0c179b2c0c2422fb30d
Jon <xiaoqiao11770@gmail.com>
2017-03-28 23:04:54 +0800

    test_22

--- app.py
+++ app.py
@@ -2,10 +2,15 @@
 from flask import Flask, request
 import hashlib
 
-import config
-import function.tuling as tuling
-from function.messages import receive, reply
-from function.nodes import read
+# import config
+# import function.tuling as tuling
+# from function.messages import receive, reply
+# from function.nodes import read
+
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
+from wechatpy import parse_message
+from wechatpy.replies import TextReply
 
 app = Flask(__name__)
 
@@ -36,43 +41,54 @@ def weixin():
             map(sha1.update, list)
             hashcode = sha1.hexdigest()
             print "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
-            else:
-                return ""
+            # if hashcode == signature:
+            #     return echostr
+            # else:
+            #     return ""
+            try:
+                check_signature(token, signature, timestamp, nonce)
+            except InvalidSignatureException:
+                print 'Authentication failed'
+                return ''
         else:
             print '=' * 20, 'POSE', '=' * 20
             webData = request.stream.read()
             print "Handle Post webdata is ", webData   #后台打日志
-            recMsg = receive.parse_xml(webData)
-            if isinstance(recMsg, receive.Msg):
-                toUser = recMsg.FromUserName
-                print "to User ====>", toUser
-                fromUser = recMsg.ToUserName
-                print "from User ====>", fromUser
-                if recMsg.MsgType == 'text':
-                    rec_content = recMsg.Content
-                    content = read.type_nodes(rec_content)
-                    if not content:
-                        try:
-                            node_type, node_name = read.find_node(rec_content)
-                            node_hand = read.Read(node_type, node_name).hand()
-                            node_bewirte = read.Read(node_type, node_name).bewirte()
-                            content = node_hand + "\n" + "-------" + "\n" + node_bewirte
-                        except:
-                            content = tuling.result(rec_content)
-                        content = content.encode('utf-8')
-                    replyMsg = reply.TextMsg(toUser, fromUser, content)
-                    return replyMsg.send()
-                if recMsg.MsgType == 'image':
-                    mediaId = recMsg.MediaId
-                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
-                    return replyMsg.send()
-
-                else:
-                    return reply.Msg().send()
-            else:
-                print "暂且不处理"
-                return "success"
+            # recMsg = receive.parse_xml(webData)
+            msg = parse_message(webData)
+            # if isinstance(recMsg, receive.Msg):
+            #     toUser = recMsg.FromUserName
+            #     print "to User ====>", toUser
+            #     fromUser = recMsg.ToUserName
+            #     print "from User ====>", fromUser
+            #     if recMsg.MsgType == 'text':
+            #         rec_content = recMsg.Content
+            #         content = read.type_nodes(rec_content)
+            #         if not content:
+            #             try:
+            #                 node_type, node_name = read.find_node(rec_content)
+            #                 node_hand = read.Read(node_type, node_name).hand()
+            #                 node_bewirte = read.Read(node_type, node_name).bewirte()
+            #                 content = node_hand + "\n" + "-------" + "\n" + node_bewirte
+            #             except:
+            #                 content = tuling.result(rec_content)
+            #             content = content.encode('utf-8')
+            #         replyMsg = reply.TextMsg(toUser, fromUser, content)
+            #         return replyMsg.send()
+            #     if recMsg.MsgType == 'image':
+            #         mediaId = recMsg.MediaId
+            #         replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+            #         return replyMsg.send()
+            #
+            #     else:
+            #         return reply.Msg().send()
+            reply = TextReply(message=msg)
+            reply.content = 'text reply'
+            # 转换成 XML
+            xml = reply.render()
+            return xml
+            # else:
+            #     print "暂且不处理"
+            #     return "success"
     except Exception, Argument:
         return Argument
--- config.py
+++ config.py
@@ -18,7 +18,7 @@ def wechat_config():
     weconf = dict(
     token='test_houdini',
     appid='wx332e0321845c6e3c',
-    appsecret='e2bb9f5d4af14943d8c0121d61cf50ed',
+    appsecret='c07f2bb1079e316d5dc65401ba6e7322',
     encrypt_mode='safe',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
     encoding_aes_key='XUi5VjeFWb7v5LFVIFOwL0iT0fQLTQv3TkLrZfigu6h'  # 如果传入此值则必须保证同时传入 token, appid
     )
